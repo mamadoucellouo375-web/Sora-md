22f26eb2646c2c7cafc6a3967bc0a20c656f4f08
mamadoucellouo375-web <mamadoucellouo375@gmail.com>
2026-07-26 23:43:15 +0000

    Update bug.js

--- commands/bug.js
+++ commands/bug.js
@@ -1,73 +1,14 @@
 async function bug(message, client, texts, num) {
-
     try {
-        
-            const remoteJid = message.key?.remoteJid;
+        const remoteJid = message.key?.remoteJid;
 
-            await client.sendMessage(remoteJid, {
-
-                image: { url: `database/${num}.jpg` },
-
-                caption: `> ${texts}`,
-
-                contextInfo: {
-
-                    externalAdReply: {
-
-                        title: "Join Our WhatsApp Channel",
-
-                        body: " SORA MD ",
-
-                        mediaType: 1, // Image preview
-
-                        thumbnailUrl: `https://whatsapp.com/channel/0029VbBT7FdLCoX1TDyQQb1B`,
-
-                        renderLargerThumbnail: false,
-
-                        mediaUrl: `${num}.jpg`,
-
-                        sourceUrl: `${num}.jpg`
-                    }
-                }
-            });
+        await client.sendMessage(remoteJid, {
+            text: `> ${texts}`
+        });
 
     } catch (e) {
-     console.log(e)
-
+        console.log(e)
     }
 }
 
-
-
-
-            /*const remoteJid = message.key.remoteJid;
-
-            await client.sendMessage(remoteJid, {
-
-                image: { url: `${num}.jpg` },
-
-                caption: `> ${texts}`,
-
-                contextInfo: {
-
-                    externalAdReply: {
-
-                        title: "Join Our WhatsApp Channel",
-
-                        body: " jєαn-s dєv | σвítσ dєv  ",
-
-                        mediaType: 1, // Image preview
-
-                        thumbnailUrl: `https://whatsapp.com/channel/0029Vb40oQM7z4kmL0SG4h45`,
-
-                        renderLargerThumbnail: false,
-
-                        mediaUrl: `${num}.jpg`,
-
-                        sourceUrl: `${num}.jpg`
-                    }
-                }
-            });
-        }
-        */
-        export default bug;
+export default bug;
